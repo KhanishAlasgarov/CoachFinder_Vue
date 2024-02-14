bf9339ac66f07f7d302e69b37b750b097d2d9922
KhanishAlasgarov <khanisha@code.edu.az>
2024-02-14 20:59:58 +0400

    feat: coache register form is ready

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>
-        Register as a coach
-    </div>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!!</h2>
+      <coache-register-form> </coache-register-form
+    ></base-card>
+  </section>
 </template>
+
+<script>
+import CoacheRegisterForm from '../../components/coaches/CoacheRegisterForm.vue';
+export default {
+  components: {
+    CoacheRegisterForm,
+  },
+};
+</script>
