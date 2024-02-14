15466cce3bc146866cbe62101f4e40403f4448b4
KhanishAlasgarov <khanisha@code.edu.az>
2024-02-14 15:34:43 +0400

    feat: filtering feature has been added, but it is not very good, it will be renewed as soon as possible

---
+++ src/components/coaches/CoacheFilter.vue
@@ -0,0 +1,70 @@
+<template>
+  <base-card>
+    <h2>Find your Coach</h2>
+    <span class="filter-option">
+      <input type="checkbox" id="frontend" @change="setFilter" checked />
+      <label for="frontend">Frontend</label>
+    </span>
+
+    <span class="filter-option">
+      <input type="checkbox" @change="setFilter" id="backend" checked />
+      <label for="backend">Backend</label>
+    </span>
+
+    <span class="filter-option">
+      <input type="checkbox" @change="setFilter" id="career" checked />
+      <label for="career">Career</label>
+    </span>
+  </base-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      filters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
+  },
+  emits: ['change-filter'],
+  methods: {
+    setFilter(event) {
+      const id = event.target.id;
+      const isActive = event.target.checked;
+
+      this.filters = {
+        ...this.filters,
+        [id]: isActive,
+      };
+
+      this.$emit('change-filter', this.filters);
+    },
+  },
+};
+</script>
+<style scoped>
+h2 {
+  margin: 0.5rem 0;
+}
+
+.filter-option {
+  margin-right: 1rem;
+}
+
+.filter-option label,
+.filter-option input {
+  vertical-align: middle;
+}
+
+.filter-option label {
+  margin-left: 0.25rem;
+  user-select: none;
+}
+
+.filter-option.active label {
+  font-weight: bold;
+}
+</style>
--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -1,10 +1,14 @@
 <template>
-  <section>Filter</section>
+  <section>
+    <coache-filter @change-filter="setFilters"></coache-filter>
+  </section>
   <section>
     <base-card>
       <div class="controls">
         <base-button mode="outline">Refresh</base-button>
-        <base-button mode="outline" link :to="{name:'register'}">Register as Coach</base-button>
+        <base-button mode="outline" link :to="{ name: 'register' }"
+          >Register as Coach</base-button
+        >
       </div>
       <ul v-if="hasCoaches">
         <coache-list-item
@@ -27,14 +31,42 @@
 <script>
 import { mapGetters } from 'vuex';
 import CoacheListItem from '../../components/coaches/CoacheListItem.vue';
+import CoacheFilter from '../../components/coaches/CoacheFilter.vue';
 export default {
   components: {
     CoacheListItem,
+    CoacheFilter,
+  },
+  data() {
+    return {
+      filters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
   },
   computed: {
     ...mapGetters('coaches', ['hasCoaches']),
     filteredCoaches() {
-      return this.$store.getters['coaches/getCoaches'];
+      const coaches = this.$store.getters['coaches/getCoaches'];
+      return coaches.filter((coach) => {
+        if (this.filters.frontend && coach.areas.includes('frontend')) {
+          return true;
+        }
+        if (this.filters.backend && coach.areas.includes('backend')) {
+          return true;
+        }
+        if (this.filters.career && coach.areas.includes('career')) {
+          return true;
+        }
+        return false;
+      });
+    },
+  },
+  methods: {
+    setFilters(updatedFilters) {
+      this.filters = updatedFilters;
     },
   },
 };
