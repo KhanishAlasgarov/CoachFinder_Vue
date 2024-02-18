c37f10f8fc81b82cc1cb378e2340882ca5adcb07
KhanishAlasgarov <khanisha@code.edu.az>
2024-02-18 15:44:49 +0400

    feat: auth module added to the vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,17 @@ import { createStore } from 'vuex';
 
 import coaches from './modules/coaches/index';
 import requests from './modules/requests/index';
+import auth from './modules/auth';
 
 export default createStore({
   modules: {
     coaches,
     requests,
+    auth,
   },
-  state(){
-    return{
-      userId:'c3'
-    }
-  }
-  
+  state() {
+    return {
+      userId: 'c3',
+    };
+  },
 });
